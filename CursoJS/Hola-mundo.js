63c3f7f5c9f41a14b1809249b3bd709be8499dca
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-03 13:34:11 -0600

    Updated

--- CursoJS/Hola-mundo.js
+++ CursoJS/Hola-mundo.js
@@ -1,1 +1,10 @@
-console.log("Holaa Nefta SIuuu")
+console.log( "Holaa Nefta SIuuu")
+
+function sumaDosnum(number1,number2){
+
+    suma=number1*number2;
+    return suma;
+}
+
+let resultado = sumaDosnum(100,500)
+console.log(resultado)
