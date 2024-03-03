9573b54472af155d40a41cfe1bcee98e04233dd4
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-03 13:54:40 -0600

    Updated

---
+++ CursoJS/operadores.js
@@ -0,0 +1,31 @@
+//Operadores aritmeticos
+
+let a, b ,c ,d,e,f,g;
+
+// Suma
+
+a = 3+4;
+console.log(a)
+
+//Resta
+
+b= 100-500
+console.log(b)
+
+//Multiplicacion
+
+c = 50*20
+console.log(c)
+
+// divicion
+d=20/2.25
+console.log(d)
+
+//Modulo
+f=10%2
+console.log(f)
+
+//Potencia
+
+g=5**3
+console.log(g)
