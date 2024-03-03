e91069b03c59f7ce0ea56d0ede59c36f4f9989c5
Nefta11 <neftaliarturohernandez@gmail.com>
2024-03-03 13:40:35 -0600

    Concepto de hoisting

---
+++ CursoJS/hoisting.js
@@ -0,0 +1,11 @@
+// Hoisting
+//Signica que podemos primero utilizar una variavle y despues utilizarla
+
+//Ejemplo
+
+x=90; //2. Inicializamos la variable
+console.log(x)
+var x ; //1. declarar
+
+// solo funciona con la declaracion var
+// y eso no es recomendable y por eso con let no aplica el concepto
