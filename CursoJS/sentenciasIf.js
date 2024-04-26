316f20758685b676929dc28bd81dd27411829214
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-26 14:24:38 -0600

    Updated

--- CursoJS/sentenciasIf.js
+++ CursoJS/sentenciasIf.js
@@ -21,3 +21,13 @@ else if (miNumero < 0){
 //sirve para simplificar toda la sentencia anterior obvio si el codigo es corto
 
 miNumero<0?console.log("positivo"):console.log("negativo")
+
+
+//Algoritmo para saber si una persona es mayor de edad
+
+const EDAD_ADULTO =18;
+let edadPersona=30;
+
+if(edadPersona >= EDAD_ADULTO){
+    console.log("La persona es mayor de edad tiene una deda de : ", edadPersona)
+}
