146088daa8e35959b30ccd8073930c9e5bfecf3a
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-29 16:23:57 -0600

    Updated

--- CursoJS/Funciones/funciones2.js
+++ CursoJS/Funciones/funciones2.js
@@ -34,3 +34,31 @@ variableGlobal = 33;
 //No podemos redefinir una varibale global (let)
 }
 miFuncion(variableGlobal);
+
+
+
+//////////////////////////////////////////////////////////////////
+//Funciones recursivas
+//se llaman asi misma 
+//debe de avanzar a un caso base para no caer en ciclos infinitos
+
+//Ejercicios de recursividad
+
+//Imprimir 3,2,1 
+
+function funcionRecursiva(numero){
+    //Caso base
+
+    if(numero == 1)
+        console.log(numero);
+    else{
+        console.log(numero);
+        funcionRecursiva(numero - 1);
+    }
+}
+
+//LLamamos a nuestra funcion
+
+funcionRecursiva(3);
+
+
