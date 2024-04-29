e80065cb80e3763144ebbcadcaf091d8f535c8cc
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-28 23:00:52 -0600

    Updated

--- CursoJS/sentenciaSwitch.js
+++ CursoJS/sentenciaSwitch.js
@@ -1,2 +1,38 @@
 //Sentencias Switch en js
 
+// dias de la semana con switch
+
+let diaSemana = 2;
+
+switch(diaSemana){
+    case 1: // dia semana == 1
+    console.log("Lunes")
+    break;
+
+    case 2:
+        console.log("Martes")
+        break;
+
+    case 3:
+        console.log("Miercoles")
+        break;
+
+    case 4:
+        console.log("Jueves")
+    break;
+
+    case 5:
+        console.log("Viernes")
+        break;
+    
+    case 6:
+        console.log("Sabado")
+    break;
+
+    case 7:
+        console.log("Domingo")
+    break;
+
+    default:
+        console.log(`Dia no exixtente ${diaSemana}`)
+}
