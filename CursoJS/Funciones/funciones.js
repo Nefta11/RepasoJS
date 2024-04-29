a97eeb9e4e61c75e73b3dc64bf7a753fc1c1e0c8
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-29 15:43:42 -0600

    Updated

--- CursoJS/Funciones/funciones.js
+++ CursoJS/Funciones/funciones.js
@@ -2,3 +2,21 @@
 // Una función es un bloque de codigo reutilizable, que realiza una tarea en particular
 //Puede tener valores de entrada como de salida estos son opcionales
 
+
+//Tipos de funciones
+
+// Funciones definidas por el usuario
+
+/// Funciones Incorporadas (Fun de cadenas de caracteres , Funciones Matematicas)
+
+
+
+////Eje,plo de procedimiento en JS
+//Definir procedimiento
+
+function saludar(mensaje){
+    //Cuerpo  de la funcion
+console.log(`Mensaje: ${mensaje}`)
+
+}
+saludar('Holaa Bobos Siuuu')
