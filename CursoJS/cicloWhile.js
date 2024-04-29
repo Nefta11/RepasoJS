99f43188390517a455517de520e3117f819ccbae
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-29 11:04:42 -0600

    Updated

--- CursoJS/cicloWhile.js
+++ CursoJS/cicloWhile.js
@@ -2,10 +2,23 @@
 
 //imprimir los valores de 0 a 100
 
-let contador = 0, repeticion = 15 ;
+let contador = 0, repeticion = 5 ;
 
 while(contador <= repeticion){
 console.log(contador++);
 
 //contador++;
 }
+
+
+
+
+//// ciclo do while
+// primero se ejecuta el codigo por lo menos una vez y despues de verifica la condicion
+
+
+do{
+
+    console.log(contador++);
+
+}while(contador <= repeticion)
