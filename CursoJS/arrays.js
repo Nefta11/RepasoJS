379b0d44a4685ca026bae48399e5a335620ad3a3
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-29 14:45:45 -0600

    Updated

--- CursoJS/arrays.js
+++ CursoJS/arrays.js
@@ -12,3 +12,19 @@ numerosArray[4]='Hola Siuu'
 console.log(`Elemento 1 - [0]: ${numerosArray[0]}`)
 console.log(`Elemento 2 - [2]: ${numerosArray[2]}`)
 console.log(`Elemento 4 - [0]: ${numerosArray[4]}`)
+
+
+//Arreglo declarado yaa
+
+let numerosArreglo = [12,11,155,0,0]
+
+console.log(`Elemento 1 - [0]: ${numerosArreglo[0]}`)
+console.log(`Elemento 2 - [2]: ${numerosArreglo[2]}`)
+console.log(`Elemento 4 - [0]: ${numerosArreglo[4]}`)
+
+
+//Iterar elementos de un arreglo
+
+for (let i =0; i < numerosArreglo.length; i++){
+console.log(numerosArreglo[i])
+}
