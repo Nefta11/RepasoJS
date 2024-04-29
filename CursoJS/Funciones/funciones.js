dfbece179a249ec602f3f90df68b195f926d1e3b
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-29 15:50:15 -0600

    Updated

--- CursoJS/Funciones/funciones.js
+++ CursoJS/Funciones/funciones.js
@@ -12,11 +12,26 @@
 
 
 ////Eje,plo de procedimiento en JS
-//Definir procedimiento
+//1.Definir procedimiento
 
 function saludar(mensaje){
     //Cuerpo  de la funcion
 console.log(`Mensaje: ${mensaje}`)
 
 }
+//2. Llamar el procediemiento
 saludar('Holaa Bobos Siuuu')
+
+
+//Ejemplo de funciones JS
+
+//1. Declarar la funci
+
+function suma(a,b){
+    let resultado = a + b;
+    return resultado
+}
+let argA = 6; argB = 8;
+
+let resultado=suma(argA, argB)
+console.log(`Resultado de la suma ${argA} + ${argB} = ${resultado}`)
