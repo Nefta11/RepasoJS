a715b90eeb162af0c15f9a895faf9af67980c076
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-28 22:47:40 -0600

    Updated

--- CursoJS/sentenciasIf.js
+++ CursoJS/sentenciasIf.js
@@ -30,3 +30,32 @@ if(edadPersona >= EDAD_ADULTO){
 }else{
     console.log("La persona es menor de edad")
 }
+
+
+// Dias de la semana
+
+let diaSemana = 8;
+
+if (diaSemana == 1){
+    console.log("Lunes")
+}
+else if (diaSemana == 2){
+    console.log("Martes")
+}
+else if (diaSemana == 3){
+    console.log("Miercoles")
+}
+else if (diaSemana == 4){
+    console.log("Jueves")
+}
+else if (diaSemana == 5){
+    console.log("Viernes")
+}
+else if (diaSemana == 6){
+    console.log("Sabado")
+}
+else if (diaSemana == 7){
+    console.log("Domingo")
+}
+else
+    (`Dia erroneo ${diaSemana}`)
