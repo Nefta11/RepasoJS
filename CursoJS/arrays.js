ee43fd05d73b3991cdaba38cb27d179de2f33497
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-29 12:33:27 -0600

    Updated

---
+++ CursoJS/arrays.js
@@ -0,0 +1,14 @@
+//Arreglos en js
+
+let numerosArray = [];
+
+//Modificar valores de nuestro arreglos
+
+numerosArray[0]=13;
+numerosArray[1]=21;
+numerosArray[4]='Hola Siuu'
+
+//Leer los valores del arreglo
+console.log(`Elemento 1 - [0]: ${numerosArray[0]}`)
+console.log(`Elemento 2 - [2]: ${numerosArray[2]}`)
+console.log(`Elemento 4 - [0]: ${numerosArray[4]}`)
