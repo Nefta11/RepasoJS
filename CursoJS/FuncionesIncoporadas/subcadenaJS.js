e54fe52c8ef81190c63028f3c15cdf9ee79eded6
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-19 14:46:10 -0600

    Updated

---
+++ CursoJS/FuncionesIncoporadas/subcadenaJS.js
@@ -0,0 +1,14 @@
+//Subcadena en JS
+let cadena3 = 'Yo soy el numero 1 siuu Perros'
+
+//Substring(indice_inicio, indice;fin - 1)
+//No se incluye el caracter indice_fin
+
+let cadena1 = cadena3.substring(0,9)
+let cadena2 = cadena3.substring(9, 24) //Esta funcion extrae del iundice 9 asta el fianl de la misma
+let cadena0 = cadena3.substring(9)
+
+
+console.log(cadena1)
+console.log(cadena2)
+console.log(cadena0)
