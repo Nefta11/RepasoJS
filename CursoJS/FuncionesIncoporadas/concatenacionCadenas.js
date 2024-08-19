458b11137b2131d64ac4eda12ff3fe0b42c8d619
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-19 14:55:09 -0600

    add Funciones

---
+++ CursoJS/FuncionesIncoporadas/concatenacionCadenas.js
@@ -0,0 +1,9 @@
+//Concatenacion de cadenas en js
+// De esta manera se concatenan cadenas siuuu
+let cadenaOne = 'Puto'
+let cadenaTwo = 'el que lo lea'
+
+let cadenaConca = cadenaOne + ' ' + cadenaTwo
+console.log(cadenaConca)
+
+console.log(`${cadenaOne} ${cadenaTwo}`)
