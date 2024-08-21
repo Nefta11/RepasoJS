3b6ec4712bcb051bb500b58250b19c622dc68623
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-21 13:01:51 -0600

    Update

--- CursoJS/ObjetosJS/constructores.js
+++ CursoJS/ObjetosJS/constructores.js
@@ -4,17 +4,28 @@ function Persona(nombre, apellido, email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+
+    this.nombreCompleto = function(){
+        return this.nombre + " " + this.apellido
+    }
 }
 
 let padre = new Persona("Arturo", "Bartolo","artbar@gmail.com");
 console.log(padre)
+console.log(padre.nombreCompleto())
 
 let madre = new Persona("Rosario", "Vergara", "Chayo@gmail.com") ;
 console.log(madre)
+console.log(madre.nombreCompleto())
+
 
 let hijo = new Persona ("Neftali", "Vergara", "neftaliarturohernandez@gmail.com");
 console.log(hijo)
+console.log(hijo.nombreCompleto())
+
+
 
 padre.apellido="Hernandez"
 
 console.log(padre)
+
