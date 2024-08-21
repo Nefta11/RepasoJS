ef806f1acc982a4b903908ef75cfcb84d0cb6d1d
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-20 19:14:21 -0600

    Distintas maneras de imprimir nuestros objetos

--- CursoJS/ObjetosJS/AddProperties.js
+++ CursoJS/ObjetosJS/AddProperties.js
@@ -16,3 +16,27 @@ console.log(RealMdrid)
 delete RealMdrid.Precidente; //De esta manera se elimina una propiedad en nuestro objeto
 
 console.log(RealMdrid)
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Formas Distintasd de Imprimir Un Objetco en Js
+
+//1. Concatenar cada valor de cada propiedad
+console.log(RealMdrid.LeyendaAbsoluta+"  Es la leyenda absoluta de "+ RealMdrid.Liga)
+
+//2. For in
+
+for (nombrePropiedad in RealMdrid){
+    console.log(RealMdrid[nombrePropiedad])
+}
+
+// 3. Objetc Value
+
+let RealMdridArray = Object.values(RealMdrid)
+console.log(RealMdridArray)
+
+// 4. JSON
+
+let RealMdridString = JSON.stringify(RealMdrid);
+console.log(RealMdridString)
