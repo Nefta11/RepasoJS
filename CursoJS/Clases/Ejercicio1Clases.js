d7a2aabc9975e0bc81f86209a74774b0da44a0c9
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-22 16:02:33 -0600

    ejercicio completado

--- CursoJS/Clases/Ejercicio1Clases.js
+++ CursoJS/Clases/Ejercicio1Clases.js
@@ -50,9 +50,17 @@ class PersonaSiuu {
 
 class EmpleadoSiu extends PersonaSiuu {
 
+        static contadorEmpleado = 1;
+
     constructor(nombre,apellido,edad,sueldo){
         super(nombre,apellido,edad)
         this._sueldo = sueldo;
+
+        this._idEmpleado = EmpleadoSiu.contadorEmpleado++;
+    }
+
+    get idEmpleado(){
+        return this._idEmpleado;
     }
 
     get sueldo(){
@@ -64,15 +72,50 @@ class EmpleadoSiu extends PersonaSiuu {
     }
 
     datosCompletos(){
-        return super.datosCompletos()+"  - "+this._sueldo;
+        return "idPersona "+super.datosCompletos()+" idEmpleado"+" "+this._idEmpleado+"  - "+this._sueldo;
     }
 }
 
+class Cliente extends PersonaSiuu{
+
+    static contadorCliente = 1
+
+    constructor(nombre,apellido,edad,fecha){
+        super(nombre,apellido,edad)
+
+        this._fecha= fecha;
+
+        this._idCliente = Cliente.contadorCliente++;
+    }
+
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fecha(){
+        return this._fecha;
+    }
+
+    set fecha(fecha){
+        this._fecha = fecha;
+    }
+
+    datosCompletos(){
+        return "idPersona "+super.datosCompletos()+" idCliente"+" "+this._idCliente+" "+" - "+this._fecha
+    }
+}
 
 
 
 persona1 = new PersonaSiuu("Neftali","Hernandez",24)
 console.log(persona1.toString())
 
+persona2 = new PersonaSiuu ("Avryl","Salas",20)
+console.log(persona2.toString())
 Empleado1 = new EmpleadoSiu("Rosario","Vergara",54, 1200)
 console.log(Empleado1.toString())
+
+nuevoCliente = new Cliente("Naruto", "Uzumaki",17,"22/08/2024")
+console.log(nuevoCliente.toString())
+
+console.log(PersonaSiuu.contador)
