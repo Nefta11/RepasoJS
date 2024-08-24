64970fb925c0837b2cabcd2a74554a8997119c0c
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-24 10:14:03 -0600

    add class Monitor

--- CursoJS/Clases/ejercicioCompu.js
+++ CursoJS/Clases/ejercicioCompu.js
@@ -44,13 +44,6 @@ class Raton extends DispositivosEntrada {
 }
 
 
-let raton1 = new Raton("USB","Hp")
-console.log(raton1.toString())
-
-let raton2 = new Raton ("Bluethoo", "Dell")
-console.log(raton2.toString())
-
-
 class Teclado extends DispositivosEntrada{
 
     static contadorTeclados = 1;
@@ -69,8 +62,58 @@ class Teclado extends DispositivosEntrada{
     }
 }
 
+
+class Monitor {
+    static contadorMonitores = 1;
+
+    constructor(marca,tamaño){
+        this._idMonitor = Monitor.contadorMonitores++;
+        this._marca=marca;
+        this._tamaño=tamaño;
+    }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca =marca;
+    }
+
+    get tamaño(){
+        return this._tamaño;
+    }
+
+    set tamaño(tamaño){
+        this._tamaño=tamaño;
+    }
+
+    toString(){
+        return `Monitor: idMonitor ${this._idMonitor}, Marca: ${this.marca}, Tamaño: ${this._tamaño}`
+    }
+}
+
+
+
+let raton1 = new Raton("USB","Hp")
+console.log(raton1.toString())
+
+let raton2 = new Raton ("Bluethoo", "Dell")
+console.log(raton2.toString())
+
+
+
 teclado1 = new Teclado ("usb","Magic")
 teclado2=new Teclado("Bluethoo","Acer")
 console.log(teclado1.toString())
 console.log(teclado2.toString())
 
+monitor1= new Monitor("Korui",22)
+monitor2=new Monitor("Samsung",17)
+
+console.log(monitor1.toString())
+console.log(monitor2.toString())
