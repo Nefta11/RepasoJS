834ce6826ba2f06ecd166f9c4872d4ba988a484a
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-24 10:49:25 -0600

    add use strict

---
+++ CursoJS/usoModoestricto.js
@@ -0,0 +1,10 @@
+//para utilizar el modo stricto lo hacemos con la palabra "use strict"
+
+"use strict"
+
+x = 10
+
+// de esta manera me maracara error por que la variable no ha sido declarada anteriormente
+
+let x= 11
+console.log(x)
