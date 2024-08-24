d747dd7931e6d50a42940f5de8cadd89b3a1af4c
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-24 10:25:08 -0600

    add class computadora

--- CursoJS/Clases/ejercicioCompu.js
+++ CursoJS/Clases/ejercicioCompu.js
@@ -98,6 +98,22 @@ class Monitor {
 }
 
 
+class Computadora{
+
+    static contadorComputadoras = 1;
+
+    constructor(nombre,monitor,teclado,raton){
+        this._idComputadora = Computadora.contadorComputadoras++;
+        this._nombre = nombre;
+        this._monitor=monitor;
+        this._teclado=teclado;
+        this._raton=raton;
+    }
+    toString(){
+    return `Computadora-  ${this._idComputadora} ${this._nombre} \n ${this._monitor} \n ${this._raton}  \n ${this._teclado}`
+    }
+}
+
 
 let raton1 = new Raton("USB","Hp")
 console.log(raton1.toString())
