c630f2b500a601ba1f4b50dc6e2aec1198236a29
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-24 12:03:20 -0600

    add ejemplo polimorfismo

--- CursoJS/POO en js/sobreescritura.js
+++ CursoJS/POO en js/sobreescritura.js
@@ -24,6 +24,19 @@ class Gerente extends Empleado{
      }
 }
 
+//Ejemplo de polimorfismo
+function imprimir(tipo){
+console.log(    tipo.obtenerDetalles())
+    //Mulriples formas en la que se puede llamar un metodo
+    //Ya que el metodo obtenerDetalles() viene desde la clase padre Empleado pero es sobreescrita en la clase hija
+    //Por lo que dependiendo el tipo sera la forma en que se mande a llama al metodo
+
+}
+
 let gerente1= new Gerente("Neftali",27880, "Software")
+let empleado = new Empleado ("Janteh", "Amiztlan")
 
 console.log(gerente1.obtenerDetalles())
+
+imprimir(empleado)
+imprimir (gerente1)
