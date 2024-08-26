c18fa55c49dad249e49493c56e6dc2df4c3660e9
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-25 19:19:59 -0600

    add funcion flecha

---
+++ CursoJS/Funciones Flecha/funcionFlecha.js
@@ -0,0 +1,13 @@
+// uso de las funciones de flecha
+function miFuncion(){
+    console.log("Ejmeplo funcion simple")
+}
+
+miFuncion();
+
+//Funcion flecha
+let miFuncionFlecha = () =>{
+console.log("Hola contenido de la funcion flecha")
+}
+
+miFuncionFlecha();
