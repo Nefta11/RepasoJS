eb2ef1ef02ca3fcffb6c60f49179cd5b7fe14ebe
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-25 18:56:49 -0600

    try catch finally

--- CursoJS/POO en js/manejoDeErrores.js
+++ CursoJS/POO en js/manejoDeErrores.js
@@ -1,1 +1,20 @@
 //Manjeo de errores en js
+
+"use strict"
+
+// ejemplo de uso de try
+
+try {
+    mifuncion()
+    x = 10;
+} catch (error) {
+    console.log(error)
+}
+finally{
+    console.log("Termina larevision de errores")
+} // finally siempre se ejecuta es opcional
+console.log("continuamos...")
+
+//En este ejmplo podemos ver que al usar el modo estricto de js no nos deja utilizar una variables sin antes haberla declarado
+//por lo que usamos el try, catch 
+
