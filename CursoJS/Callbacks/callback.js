78f33b325597cbf3919d5b48b5cc7bf65f9687f7
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-25 19:46:18 -0600

    add ejemplo funcion callbacks

---
+++ CursoJS/Callbacks/callback.js
@@ -0,0 +1,32 @@
+//Una funcion del tipo callbacks es un funcion que se pasa como parametro a otra funcion 
+// dentro de una funcion podemos mandar a llamar otra funcion
+// recibiendo como argumento esa funcin
+
+
+function miFuncion(){
+    console.log("Funcion 1")
+}
+
+function miFuncion2(){
+    console.log("Mi funcion2")
+}
+
+
+miFuncion2()
+miFuncion()
+//se ejecutan de manera secuencial en js
+
+
+
+// Funciones de tipo callbacks
+
+function imprimir(mensaje){
+    console.log(mensaje)
+}
+
+function sumar(op1,op2, funcionCallback){
+    let resultado = op1 + op2
+    funcionCallback(`resultado: ${resultado}`)
+}
+
+sumar(11,7,imprimir);
